7edd8087641ea63520c2241644069242ea3b8dbf
mulder974 <pierrekuh@yahoo.fr>
2023-05-10 00:48:58 +0200

    m

--- api.py
+++ api.py
@@ -29,8 +29,11 @@ def get_external_api_data(url: str):
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+return {"Hello": "World"}
 
+@app.get("/hello")
+def read_root():
+    return {"Hello": "World"}
 
 @app.post("/send_qr")
 async def send_qr(request: SendQRRequest):
