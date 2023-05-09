dfa8a5408c2dbfaaaa7c62c8e737863d9cc57e3c
mulder974 <pierrekuh@yahoo.fr>
2023-05-09 13:31:43 +0200

    refacto api.py

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from pydantic import BaseModel
 import db
 import secure
 from secure import TokenData
@@ -11,40 +12,42 @@ from fastapi import FastAPI, Depends, HTTPException, Header
 app = FastAPI()
 security = HTTPBearer()
 
+class SendQRRequest(BaseModel):
+    username: str
+    pwd: str
+
+def get_external_api_data(url: str):
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        raise HTTPException(status_code=500, detail="Unable to fetch data from external API")
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 @app.post("/send_qr")
-async def send_qr(username,pwd):
-
-    pwd_in_db = db.get_user_pwd(username)[0]
-    print(pwd_in_db)
+async def send_qr(request: SendQRRequest):
+    pwd_in_db = db.get_user_pwd(request.username)[0]
 
     # Vérifier que les informations d'identification sont valides
-    if not secure.verify_pwd(pwd, pwd_in_db) :
+    if not secure.verify_pwd(request.pwd, pwd_in_db):
         raise HTTPException(status_code=401, detail="Nom d'utilisateur ou mot de passe incorrect")
-
     else:
-        token = secure.generate_token(username)
+        token = secure.generate_token(request.username)
         qr = qr_code.create_qr_code(token)
-        mail.send_email(qr,"hugo.fugeray@gmail.com")
+        mail.send_email(qr, "hugo.fugeray@gmail.com")
 
     return "Email sent"
-
 
 # Endpoint protégé par un jeton
 @app.get("/products")
 async def get_product(token_data: TokenData = Depends(secure.verify_jwt_token)):
-
     url = "https://615f5fb4f7254d0017068109.mockapi.io/api/v1/products"
-    response = requests.get(url)
-    if response.status_code == 200:
-        # Parse the JSON data from the response
-        data = response.json()
-
+    data = get_external_api_data(url)
     return data
-
 
 @app.get("/validate-token")
 async def validate_token(authorization: str = Header(...)):
@@ -62,9 +65,5 @@ async def validate_token(authorization: str = Header(...)):
 @app.get("/products/{product_id}")
 async def get_product_by_id(product_id: int, token_data: TokenData = Depends(secure.verify_jwt_token)):
     url = f"https://615f5fb4f7254d0017068109.mockapi.io/api/v1/products/{product_id}"
-    response = requests.get(url)
-    if response.status_code == 200: # Parse the JSON data from the response data = response.json() return data
-        data = response.json()
+    data = get_external_api_data(url)
     return data
-
-
