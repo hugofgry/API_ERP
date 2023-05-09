c18ad041050bc429f33cc6cdf3d9d39393f5d6fd
mulder974 <pierrekuh@yahoo.fr>
2023-05-09 16:15:57 +0200

    m

--- api.py
+++ api.py
@@ -70,7 +70,7 @@ async def get_product_by_id(product_id: int, token_data: TokenData = Depends(sec
 
 
 @app.get("/products/search/{name}{price}")
-async def get_product_by(name: str, price:str, token_data: TokenData = Depends(secure.verify_jwt_token)):
+async def get_product_by(name: str = None, price: str = None, token_data: TokenData = Depends(secure.verify_jwt_token)):
     url = f"https://615f5fb4f7254d0017068109.mockapi.io/api/v1/products/"
     data = get_external_api_data(url)
 
