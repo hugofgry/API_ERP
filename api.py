d0cecfb182519a27f43629c5710940706420a245
mulder974 <pierrekuh@yahoo.fr>
2023-05-11 01:18:39 +0200

    m

--- api.py
+++ api.py
@@ -57,7 +57,8 @@ async def get_product_by_id(product_id: int, token_data: TokenData = Depends(sec
 async def validate_token(token_data: TokenData = Depends(secure.verify_jwt_token)):
 
     # Vérifiez si le jeton est révoqué
-    revoked_token = db.check_revoked_token(token_data.sub.split(" ")[1])
+    print(token_data.sub)
+    revoked_token = db.check_revoked_token(token_data.sub)
     if revoked_token:
         raise HTTPException(status_code=401, detail="Token has been revoked")
 
