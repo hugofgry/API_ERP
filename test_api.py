9be99786dfcb801fd2dc73d91ec5d0aae8bf3430
FUGERAY <hugo.fugeray@gmail.com>
2023-05-11 02:22:08 +0200

    testV1

--- test_api.py
+++ test_api.py
@@ -8,7 +8,7 @@ API_BASE_URL = "http://127.0.0.1:8080"
 def test_read_root():
     response = httpx.get(f"{API_BASE_URL}/")
     assert response.status_code == 200
-    assert response.json() == {"Hello": "World"}
+
 
 def test_validate_token_invalid():
     response = httpx.get(f"{API_BASE_URL}/validate-token", headers={"Authorization": "Bearer invalid_token"})
