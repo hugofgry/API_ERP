fcc6ba9883d361f1b99db2d0e4d9f0e2f5833152
FUGERAY <hugo.fugeray@gmail.com>
2023-05-11 02:05:10 +0200

    testV1

--- test_api.py
+++ test_api.py
@@ -1,31 +1,26 @@
-import pytest
 import httpx
+import pytest
+from fastapi.testclient import TestClient
+from main import app
 
 BASE_URL = "http://127.0.0.1:8080"
 
-
 def test_read_root():
-    with httpx.Client() as client:
-        response = client.get(f"{BASE_URL}/")
-        assert response.status_code == 200
-        assert response.json() == {"Hello": "World"}
+    response = httpx.get(f"{API_BASE_URL}/")
+    assert response.status_code == 200
+    assert response.json() == {"Hello": "World"}
 
+def test_validate_token_invalid():
+    response = httpx.get(f"{API_BASE_URL}/validate-token", headers={"Authorization": "Bearer invalid_token"})
+    assert response.status_code == 401
+    assert response.json() == {"detail": "Invalid token."}
 
-def test_get_products():
-    # Ajoutez un jeton JWT valide à la place de "your_valid_jwt_token_here"
-    token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJwaWVycmVzaW1wbG9uOTc0QGdtYWlsLmNvbSIsImV4cCI6MTY4NDk2ODExNn0.3h2-FqcRpqKDXpULRCUocSru5pjCQtt0q5CsQS8RfK8"
-    with httpx.Client() as client:
-        response = client.get(f"{BASE_URL}/products", headers={"Authorization": f"Bearer {token}"})
-        assert response.status_code == 200
-        assert isinstance(response.json(), list)
+def test_send_qr_invalid_credentials():
+    response = httpx.post(f"{API_BASE_URL}/send_qr", json={"username": "invalid_user", "pwd": "invalid_password"})
+    assert response.status_code == 401
+    assert response.json() == {"detail": "Nom d'utilisateur ou mot de passe incorrect"}
 
-
-def test_get_product_by_id():
-    # Ajoutez un jeton JWT valide à la place de "your_valid_jwt_token_here"
-    token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJwaWVycmVzaW1wbG9uOTc0QGdtYWlsLmNvbSIsImV4cCI6MTY4NDk2ODExNn0.3h2-FqcRpqKDXpULRCUocSru5pjCQtt0q5CsQS8RfK8"
-    product_id = 1
-    with httpx.Client() as client:
-        response = client.get(f"{BASE_URL}/products/{product_id}", headers={"Authorization": f"Bearer {token}"})
-        assert response.status_code == 200
-        assert "name" in response.json()
-        assert "details" in response.json()
+def test_revoke_token_invalid():
+    response = httpx.post(f"{API_BASE_URL}/revoke_token", json={"token": "invalid_token"})
+    assert response.status_code == 200
+    assert response.json() == "Erreur lors de la révocation du token"
